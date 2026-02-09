da293929b5527a7c96198181fba7f92a63eea573
Amanda <amanda@leijonborg.se>
2026-02-09 22:04:31 +0100

    Added Services

---
+++ src/Kanban/Services/KanbanState.cs
@@ -0,0 +1,40 @@
+﻿using Kanban.Models;
+
+namespace Kanban.Services
+{
+    public sealed class KanbanState
+    {
+        public List<KanbanColumn> Columns { get; } =
+    [
+        new KanbanColumn { Name = "Todo" },
+        new KanbanColumn { Name = "Doing" },
+        new KanbanColumn { Name = "Done" }
+    ];
+
+        public event Func<Task>? OnChangeAsync;
+
+        public async Task AddCardAsync(string columnName, KanbanCard card)
+        {
+            var column = Columns.First(c => c.Name == columnName);
+            column.Cards.Add(card);
+            await NotifyStateChangedAsync();
+        }
+
+
+        public async Task MoveCardAsync(string from, string to, Guid cardId)
+        {
+            var source = Columns.First(column => column.Name == from);
+            var target = Columns.First(column => column.Name == to);
+            var card = source.Cards.First(card => card.Id == cardId);
+
+            source.Cards.Remove(card);
+            target.Cards.Add(card);
+            await NotifyStateChangedAsync();
+        }
+        private async Task NotifyStateChangedAsync()
+        {
+            OnChangeAsync?.Invoke();
+        }
+    }
+}
+
---
+++ src/Kanban/Services/UserState.cs
@@ -0,0 +1,15 @@
+﻿namespace Kanban.Services
+{
+    public class UserState
+    {
+        public string Role { get; set; } = "Visitor";
+
+        public event Func<Task>? OnChangeAsync;
+
+        public async Task SetRoleAsync(string role)
+        {
+            Role = role;
+            await (OnChangeAsync?.Invoke() ?? Task.CompletedTask);
+        }
+    }
+}
