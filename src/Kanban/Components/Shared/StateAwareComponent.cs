b706d309f2ec11a75c9f971a401f695d4747f9e4
Amanda <amanda@leijonborg.se>
2026-02-09 22:03:51 +0100

    Added Shared for the components

---
+++ src/Kanban/Components/Shared/StateAwareComponent.cs
@@ -0,0 +1,31 @@
+﻿using Kanban.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Kanban.Components.Shared
+{
+    public abstract partial class StateAwareComponent : ComponentBase, IDisposable
+    {
+        [CascadingParameter] public KanbanState State { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            State.OnChangeAsync += HandleStateChangeAsync;
+        }
+
+        private async Task HandleStateChangeAsync()
+        {
+            await StateChanged();
+            await InvokeAsync(StateHasChanged);
+        }
+
+        protected virtual Task StateChanged() => Task.CompletedTask;
+
+        public void Dispose()
+        {
+            if (State is not null)
+            {
+                State.OnChangeAsync -= HandleStateChangeAsync;
+            }
+        }
+    }
+}
---
+++ src/Kanban/Components/Shared/UserStateAwareComponent.cs
@@ -0,0 +1,32 @@
+﻿using Kanban.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Kanban.Components.Shared
+{
+    public abstract class UserStateAwareComponent : ComponentBase, IDisposable
+    {
+        [CascadingParameter] public UserState User { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            User.OnChangeAsync += HandleUserChangeAsync;
+        }
+
+        protected virtual Task UserChanged() => Task.CompletedTask;
+
+        private async Task HandleUserChangeAsync()
+        {
+            await UserChanged();
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            if (User is not null)
+            {
+                User.OnChangeAsync -= HandleUserChangeAsync;
+            }
+        }
+    }
+}
+
